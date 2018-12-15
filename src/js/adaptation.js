8b29ddbf98c795a01394ed70275c60670b5171f1
remzov <remzovnik@gmail.com>
2018-12-15 22:32:06 +0300

    popups and blog-slider

---
+++ src/js/adaptation.js
@@ -0,0 +1,13 @@
+(function() {
+  document.addEventListener('DOMContentLoaded', () => {
+    if (window.matchMedia('(max-width: 640px)').matches) {
+      let blogSlideshow = document.querySelector('.js-blog-slideshow');
+      let blogSlideshowItems = document.querySelector('.js-blog-slideshow-items');
+
+      blogSlideshowItems.classList.add('uk-slideshow-items');
+      let slideshow = UIkit.slideshow(blogSlideshow, {
+        minHeight: 230
+      });
+    }
+  });
+})();
